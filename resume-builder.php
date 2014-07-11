307bd9422a2a59d34eb83be2c488dc3ffc08dff9
boxystudio <boxystudio@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-07-11 14:31:17 +0000

    Quick bug fix!
    git-svn-id: https://plugins.svn.wordpress.org/resume-builder/trunk@946924 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- resume-builder.php
+++ resume-builder.php
@@ -3,7 +3,7 @@
  * Plugin Name: Resume Builder
  * Plugin URI: http://demo.boxystudio.com/resume-builder/
  * Description: A WordPress plugin to build out your resume along with contact information and related skills
- * Version: 1.0.5
+ * Version: 1.0.6
  * Author: Boxy Studio
  * Author URI: http://boxystudio.com
  * License: GPL2
@@ -318,8 +318,11 @@ add_filter( 'template_include', 'rb_resume_templates' );
 function rb_resume_templates( $template ) {
     $post_types = array( 'rb_resume' );
 
-    if ( is_singular( $post_types ) && ! file_exists( get_stylesheet_directory() . '/single-resume.php' ) )
+    if ( is_singular( $post_types ) && !file_exists( get_stylesheet_directory() . '/single-resume.php' ) ):
         $template = plugin_dir_path( __FILE__ ).'/single-resume.php';
+    else :
+    	$template = get_stylesheet_directory() . '/single-resume.php';
+    endif;
 
     return $template;
 }

6c170a031409d0184919edccc98f6f8ddd3c2ed0
boxystudio <boxystudio@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-06-17 13:08:27 +0000

    Screenshot updates and a multisite fix
    git-svn-id: https://plugins.svn.wordpress.org/resume-builder/trunk@1182559 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- carbon-fields/carbon-i18n.php
+++ carbon-fields/carbon-i18n.php
@@ -1,9 +1,7 @@
 <?php
-$domain = 'crb';
-$locale = get_locale();
-$locale = apply_filters( 'plugin_locale', $locale, $domain );
-$mofile = $locale . '.mo';
-$lang_dir = CARBON_PLUGIN_ROOT . DIRECTORY_SEPARATOR . 'lang';
-
-load_textdomain( 'crb', $lang_dir . DIRECTORY_SEPARATOR . $mofile );
-?>
+$rb_domain = 'crb';
+$rb_locale = get_locale();
+$rb_locale = apply_filters( 'plugin_locale', $rb_locale, $rb_domain );
+$rb_mofile = $rb_locale . '.mo';
+$rb_lang_dir = CARBON_PLUGIN_ROOT . DIRECTORY_SEPARATOR . 'lang';
+load_textdomain( 'crb', $rb_lang_dir . DIRECTORY_SEPARATOR . $rb_mofile );
